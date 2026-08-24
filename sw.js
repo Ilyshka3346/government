84a36e7e25630ec049e1f967a3f534dce000c94b
Ilyshka3346 <150544619+Ilyshka3346@users.noreply.github.com>
2026-08-24 20:50:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,39 @@
-const cacheName = "majestic-lawbook-v4";
-const assets = ["./", "./index.html", "./styles.css", "./app.js", "./laws.js", "./manifest.json", "./icon.svg", "./russia-online-logo.png"];
+const cacheName = "majestic-lawbook-v5";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./laws.js",
+  "./manifest.json",
+  "./icon.svg",
+  "./russia-online-logo.png"
+];
 
 self.addEventListener("install", (event) => {
+  self.skipWaiting();
   event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key)))
-    )
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+      .then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
-  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+
+  event.respondWith(
+    fetch(event.request)
+      .then((response) => {
+        const copy = response.clone();
+        caches.open(cacheName).then((cache) => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then((cached) => cached || caches.match("./index.html")))
+  );
 });
